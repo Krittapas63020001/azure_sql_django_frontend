98846215860ffaccbb75bef7b6b191747be899b6
Raksit <raksit.c@gmail.com>
2026-01-28 00:26:59 +0700

    feat: Update frontend build dependencies, relax Node engine requirement, and adjust Express fallback route handling.

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Handle React routing, return all requests to index.html
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
