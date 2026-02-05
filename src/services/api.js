3d36cb67bd106b7f2b3fe80c394c4f525535fb87
krittapas63020001-art <krittapas63020001@gmail.com>
2026-02-05 22:10:48 +0700

    krittapas

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'krittapas-backend-ftgwh9frezgeeghu.eastasia-01.azurewebsites.net',
+  baseURL: 'https://krittapas-backend-ftgwh9frezgeeghu.eastasia-01.azurewebsites.net/',
   // baseURL: 'http://127.0.0.1:8000/api', // Local API
   headers: {
     'Content-Type': 'application/json',
